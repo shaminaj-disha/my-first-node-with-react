6efc7ce7425d56b605fe36891c2cf905f2a19df9
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-04-30 16:26:56 +0600

    first commit in client side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  // load data
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
+    // console.log(name, email);
+    // const user = {userName: name, userEmail: email}; //property and variable same na hole evabe
+    const user = { name, email, phone };
+
+    // post data to server
+    fetch('http://localhost:5000/user', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(response => response.json())
+      .then(data => {
+        const newUsers = [...users, data]; // as react immutable ager user er sathe new user eksathe add kore rakhte hobe
+        setUsers(newUsers);
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My own data : {users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='Name' required />
+        <input type="email" name='email' placeholder='Email' required />
+        <input type="number" name='phone' placeholder='Phone' required />
+        <input type="submit" value="Add User" />
+      </form>
+      <ul>
+        {users.map(user => <li key={user.id}>Id:
+          {user.id} Name: {user.name} Email: {user.email} Phone: {user.phone}</li>)}
+      </ul>
     </div>
   );
 }
